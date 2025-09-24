850d812ebba564a2cf85bb9f5f17b489702ca417
shareneo <irani.afshin@gmail.com>
2025-09-24 15:29:17 +1000

    I am done

--- macros/wca_to_columns.sql
+++ macros/wca_to_columns.sql
@@ -1,35 +1,38 @@
 {% macro wca_to_columns(raw_col='raw_line') %}
+    {% set metadata_query %}
+        select field_name, start_pos, length, type
+        from {{ ref('wca_fields') }}
+        order by start_pos
+    {% endset %}
 
-{# Query seed table for metadata #}
-{% set metadata_query %}
-    SELECT field_name, start_pos, length, type
-    FROM {{ ref('wca_fields') }}
-    ORDER BY start_pos
-{% endset %}
-
-{% set results = run_query(metadata_query).rows %}
-
-{% set col_parts = [] %}
-
-{% for row in results %}
-    {% set field_name = row[0] %}
-    {% set start_pos = row[1] %}
-    {% set length = row[2] %}
-    {% set field_type = row[3] %}
-
-    {% if field_type == 'text' %}
-        {% set part = 'RTRIM(SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ')) AS ' ~ field_name %}
-    {% elif field_type == 'number' %}
-        {% set part = 'TO_NUMBER(SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ')) AS ' ~ field_name %}
-    {% elif field_type == 'date' %}
-        {% set part = 'TO_DATE(SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ '), ''YYYYMMDD'') AS ' ~ field_name %}
+    {% if execute %}
+        {% set results = run_query(metadata_query).rows %}
+    {% else %}
+        {% set results = [] %}
     {% endif %}
 
-    {% do col_parts.append(part) %}
-{% endfor %}
+    {% set col_parts = [] %}
 
-{% do col_parts.append('source_file') %}
+    {% for row in results %}
+        {% set field_name = row[0] %}
+        {% set start_pos  = row[1] %}
+        {% set length     = row[2] %}
+        {% set field_type = row[3] %}
 
-{{ return(col_parts | join(',\n')) }}
+        {% if field_type == 'text' %}
+            {% set part = 'RTRIM(SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ')) AS ' ~ field_name %}
+        {% elif field_type == 'number' %}
+            {% set part = 'TO_NUMBER(SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ')) AS ' ~ field_name %}
+        {% elif field_type == 'date' %}
+            {% set part = 'SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ') AS ' ~ field_name %}
+        {% else %}
+            {% set part = 'SUBSTR(' ~ raw_col ~ ',' ~ start_pos ~ ',' ~ length ~ ') AS ' ~ field_name %}
+        {% endif %}
 
+        {% do col_parts.append(part) %}
+    {% endfor %}
+
+    {% do col_parts.append('source_file') %}
+
+    {{ return(col_parts | join(',\n')) }}
 {% endmacro %}
--- models/curated/claim_wca_columnar.sql
+++
@@ -1,5 +0,0 @@
-WITH staged AS (
-    SELECT *
-    FROM {{ source('wca','RAW_CLAIM_ACTIVITY') }}
-)
-
